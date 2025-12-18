fc2db60eac23403603c62529d844b6d9a92b303c
Saurav Kediaa <sauravkediaa@gmail.com>
2025-12-18 15:40:01 +0530

    Queries and DummySeedScript + Added dummySeedScript for development purposes + Added queries for querying local database

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,1 +1,24 @@
 //centralised database queries for all the modules
+
+const EMPLOYEES_KEY = "employees";
+const REVIEWS_KEY = "reviews";
+
+/* ========== EMPLOYEES ========== */
+export const getEmployees = () => {
+  return JSON.parse(localStorage.getItem(EMPLOYEES_KEY)) || [];
+};
+
+export const addEmployee = (employee) => {
+  const employees = getEmployees();
+  localStorage.setItem(EMPLOYEES_KEY, JSON.stringify([...employees, employee]));
+};
+
+/* ========== REVIEWS ========== */
+export const getReviews = () => {
+  return JSON.parse(localStorage.getItem(REVIEWS_KEY)) || [];
+};
+
+export const addReview = (review) => {
+  const reviews = getReviews();
+  localStorage.setItem(REVIEWS_KEY, JSON.stringify([...reviews, review]));
+};
