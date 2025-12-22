5cef5869ec13b611b417b86caf21684733d4ab55
Saurav Kediaa <sauravkediaa@gmail.com>
2025-12-22 23:20:23 +0530

    Rectified issue with stars not rendering properly in "AddReview" Page

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,24 +1,25 @@
-function StarRating({rating, setRating}) {
-    return (
-        <div className="star-rating">
-            {[1, 2, 3, 4, 5].map((star) => (
-                <button
-                    key={star}
-                    type="button"
-                    onClick={() => setRating(star)}
-                    style={{
-                        background: "none",
-                        border: "none",
-                        cursor: "pointer",
-                        fontSize: "32px",
-                        color: "gold"
-                    }}
-                >
-                    {star <= rating ? "★" : "☆"}
-                </button> 
-            ))}
-        </div>
-    )
+function StarRating({ rating, setRating }) {
+  return (
+    <div className="star-rating">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <button
+          key={star}
+          type="button"
+          onClick={() => setRating(star)}
+          style={{
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "32px",
+            color: "gold",
+            padding: "5px",
+          }}
+        >
+          {star <= rating ? "★" : "☆"}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default StarRating;
